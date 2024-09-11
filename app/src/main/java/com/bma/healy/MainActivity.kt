51b6170d03d09ce144d218aba6e6945bd472398e
aaaaaaaaana <rm551401@fiap.com.br>
2024-09-11 17:10:29 -0300

    tentando fazer o login

--- app/src/main/java/com/bma/healy/MainActivity.kt
+++ app/src/main/java/com/bma/healy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.bma.healy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var btnLogin:Button = findViewById(R.id.botaoLogin)
+
+
+        // adicionar evento do clique
+        btnLogin.setOnClickListener{
+
+            var intent = Intent(this, Login::class.java)
+
+            startActivity(intent)
+
+
         }
     }
 }
