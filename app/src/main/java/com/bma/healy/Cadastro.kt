f50f27d8536169a413c23f7dfa63460dc9aa243a
aaaaaaaaana <rm551401@fiap.com.br>
2024-09-12 15:35:26 -0300

    agora sim login e cadastro estao prontos

--- app/src/main/java/com/bma/healy/Cadastro.kt
+++ app/src/main/java/com/bma/healy/Cadastro.kt
@@ -1,6 +1,10 @@
 package com.bma.healy
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,27 @@ class Cadastro : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        var linkCadastro: TextView  = findViewById(R.id.linkCadastro)
+
+        var botaoCadastro: TextView = findViewById(R.id.botaoCadastro)
+
+
+        // adicionar evento do clique
+        linkCadastro.setOnClickListener{
+
+            var intent = Intent(this, Login::class.java)
+
+            startActivity(intent)
+
+        }
+
+        botaoCadastro.setOnClickListener{
+
+            var intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
         }
     }
 }
