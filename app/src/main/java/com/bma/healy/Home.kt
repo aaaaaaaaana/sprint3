2880f667686e4524b2df9411f1d34308f67b5f18
aaaaaaaaana <rm551401@fiap.com.br>
2024-09-15 16:52:25 -0300

    perfil feito

--- app/src/main/java/com/bma/healy/Home.kt
+++ app/src/main/java/com/bma/healy/Home.kt
@@ -1,46 +1,48 @@
 package com.bma.healy
 
+import android.annotation.SuppressLint
+import android.content.ClipData
 import android.content.Intent
+import android.media.RouteListingPreference.Item
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.bma.healy.databinding.ActivityMainBinding
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
 
-        var botaoPerfil: ImageView = findViewById(R.id.perfil)
 
-        var botaoResultado: ImageView = findViewById(R.id.resultado)
+    }
 
-        var botaoFormulario: ImageView = findViewById(R.id.formulario)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_nav, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-
-        // adicionar evento do clique
-        botaoPerfil.setOnClickListener{
-
-            var intent = Intent(this, Perfil::class.java)
-
-            startActivity(intent)
-        }
-
-        botaoResultado.setOnClickListener{
-
-            var intent = Intent(this, Home::class.java)
-
-            startActivity(intent)
-        }
-
-
-        botaoFormulario.setOnClickListener{
-
-            var intent = Intent(this, Formulario::class.java)
-
-            startActivity(intent)
+        when (item.itemId) {
+            R.id.formulario -> {
+                this.startActivity(Intent(this, Formulario::class.java))
+                return true
+            }
+            R.id.resultado -> {
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.perfil -> {
+                val intent = Intent(this, Perfil::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
