ae5f595071b13033a0829ca458e78bd72f55724f
aaaaaaaaana <rm551401@fiap.com.br>
2024-09-16 10:17:54 -0300

    tenatbdo fazer api

--- app/src/main/java/com/bma/healy/MainActivity.kt
+++ app/src/main/java/com/bma/healy/MainActivity.kt
@@ -2,62 +2,58 @@ package com.bma.healy
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.bma.healy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var username: EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
-//        var btnLogin:Button = findViewById(R.id.botaoLogin)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.botaoLogin.setOnClickListener(View.OnClickListener {
+            if(binding.user.text.toString() == "user" && binding.senha.text.toString() == "1234"){
+                Toast
+            }
+        })
+
+//
+//        var linkLogin: TextView = findViewById(R.id.linkLogin)
+//
+//        var botaoLogin: TextView = findViewById(R.id.botaoLogin)
 //
 //
 //        // adicionar evento do clique
-//        btnLogin.setOnClickListener{
-//
-//            var intent = Intent(this, Login::class.java)
-//
-//            startActivity(intent)
-//
-//        }
-//
-//
-//        var btnCadastro:Button = findViewById(R.id.botaoCadastro)
-//
-//
-//        // adicionar evento do clique
-//        btnCadastro.setOnClickListener{
+//        linkLogin.setOnClickListener {
 //
 //            var intent = Intent(this, Cadastro::class.java)
 //
 //            startActivity(intent)
-//
-//
-//
 //        }
-
-        var linkLogin: TextView = findViewById(R.id.linkLogin)
-
-        var botaoLogin: TextView = findViewById(R.id.botaoLogin)
-
-
-        // adicionar evento do clique
-        linkLogin.setOnClickListener{
-
-            var intent = Intent(this, Cadastro::class.java)
-
-            startActivity(intent)
-        }
-
-        botaoLogin.setOnClickListener{
-
-            var intent = Intent(this, Home::class.java)
-
-            startActivity(intent)
-        }
+//
+//        botaoLogin.setOnClickListener {
+//
+//            var intent = Intent(this, Home::class.java)
+//
+//            startActivity(intent)
+//        }
 
 
     }
